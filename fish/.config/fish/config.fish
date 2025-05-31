1320fd4132aec7381f649afe48720de7c4aefbf1
Doan Thieu <thieurom@gmail.com>
2025-06-01 00:33:04 +0700

    [fish] Remove asdf; update theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 set -Ux EDITOR nvim
 alias vi="nvim"
 alias vim="nvim"
