120c81b832ccf747f02afacf2a6a01b9a1bcf583
Doan Thieu <doanthieu@MacBook-Pro.local>
2026-05-15 21:59:14 +0700

    Add Fish shell config

---
+++ private_dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -Ux EDITOR nvim
+alias vi="nvim"
+alias vim="nvim"
+alias view="nvim -R"
+alias vimdiff="nvim -d"
