a0ce6bb883b3ddf1b56e526ac283548b1283cf6f
Thieurom <doanthieu@MacBook-Pro.local>
2026-05-16 14:52:12 +0700

    Migrate Neovim plugins to vim.pack

--- private_dot_config/nvim/lua/user/plugins.lua
+++ private_dot_config/nvim/lua/user/plugins.lua
@@ -1,93 +1,62 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+local gh = function(repo)
+    return "https://github.com/" .. repo
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(event)
+        local name = event.data.spec.name
+        local kind = event.data.kind
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+        if name == "nvim-treesitter" and (kind == "install" or kind == "update") then
+            vim.cmd.packadd("treesitter-parser-registry")
+            vim.cmd.packadd("nvim-treesitter")
+            if vim.fn.executable("tree-sitter") == 1 then
+                vim.cmd("TSUpdate")
+            end
+        end
+    end,
 })
 
--- Install plugins here
-return packer.startup(function(use)
-    use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
-    use { "nvim-tree/nvim-tree.lua" }
-    use { "nvim-lua/plenary.nvim" }
-    use { "nvim-telescope/telescope.nvim" }
-    use { "nvim-tree/nvim-web-devicons" }
-    use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
-    use { "akinsho/bufferline.nvim" }
-    use { "moll/vim-bbye" }
-    use { "windwp/nvim-autopairs" }
-    use { "numToStr/Comment.nvim" }
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end
-    }
-    use { "akinsho/toggleterm.nvim" }
+vim.pack.add({
+    { src = gh("nvim-tree/nvim-tree.lua") },
+    { src = gh("nvim-lua/plenary.nvim") },
+    { src = gh("nvim-telescope/telescope.nvim") },
+    { src = gh("nvim-tree/nvim-web-devicons") },
+    { src = gh("nvim-lualine/lualine.nvim"), version = "a52f078026b27694d2290e34efa61a6e4a690621" },
+    { src = gh("akinsho/bufferline.nvim") },
+    { src = gh("moll/vim-bbye") },
+    { src = gh("windwp/nvim-autopairs") },
+    { src = gh("numToStr/Comment.nvim") },
+    { src = gh("folke/which-key.nvim") },
+    { src = gh("akinsho/toggleterm.nvim") },
+    { src = gh("lukas-reineke/indent-blankline.nvim") },
 
-    -- Colorschemes
-    use { "EdenEast/nightfox.nvim" }
-    use { "neanias/everforest-nvim" }
-    use { "catppuccin/nvim", as = "catppuccin" }
+    { src = gh("EdenEast/nightfox.nvim") },
+    { src = gh("neanias/everforest-nvim") },
+    { src = gh("catppuccin/nvim"), name = "catppuccin" },
 
-    -- cmp plugins
-    use { "hrsh7th/nvim-cmp" } -- The completion plugin
-    use { "hrsh7th/cmp-buffer" } -- buffer completions
-    use { "hrsh7th/cmp-path" } -- path completions
-    use { "saadparwaiz1/cmp_luasnip" } -- snippet completions
-    use { "hrsh7th/cmp-nvim-lsp" }
+    { src = gh("hrsh7th/nvim-cmp") },
+    { src = gh("hrsh7th/cmp-buffer") },
+    { src = gh("hrsh7th/cmp-path") },
+    { src = gh("saadparwaiz1/cmp_luasnip") },
+    { src = gh("hrsh7th/cmp-nvim-lsp") },
 
-    -- snippets
-    use { "L3MON4D3/LuaSnip", commit = "e81cbe6004051c390721d8570a4a0541ceb0df10" }
-    use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
+    { src = gh("L3MON4D3/LuaSnip"), version = "e81cbe6004051c390721d8570a4a0541ceb0df10" },
+    { src = gh("rafamadriz/friendly-snippets") },
 
-    -- LSP
-    use { "neovim/nvim-lspconfig", commit = "b6091272422bb0fbd729f7f5d17a56d37499c54f" } -- enable LSP
-    use { "williamboman/mason.nvim" }
-    use { "williamboman/mason-lspconfig.nvim", commit = "e86a4c84ff35240639643ffed56ee1c4d55f538e" }
-    use { "nvimtools/none-ls.nvim" } -- for formatters and linters
+    { src = gh("neovim/nvim-lspconfig"), version = "b6091272422bb0fbd729f7f5d17a56d37499c54f" },
+    { src = gh("williamboman/mason.nvim") },
+    { src = gh("williamboman/mason-lspconfig.nvim"), version = "e86a4c84ff35240639643ffed56ee1c4d55f538e" },
+    { src = gh("nvimtools/none-ls.nvim") },
 
-    -- Treesitter
-    use { "nvim-treesitter/nvim-treesitter", commit = "4fd6d9dc175f367cdcec87630d8a3950ba7daef4" }
+    { src = gh("neovim-treesitter/treesitter-parser-registry") },
+    { src = gh("neovim-treesitter/nvim-treesitter") },
 
-    -- Git
-    use { "lewis6991/gitsigns.nvim" }
+    { src = gh("lewis6991/gitsigns.nvim") },
+}, {
+    confirm = false,
+    load = true,
+})
 
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
-end)
+vim.o.timeout = true
+vim.o.timeoutlen = 300
