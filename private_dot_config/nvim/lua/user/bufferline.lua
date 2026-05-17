8cd8892e6a2ea81aae56b98a1ba2e733b2b0e4a7
Thieurom <doanthieu@MacBook-Pro.local>
2026-05-17 08:10:45 +0700

    Refine Neovim statusline and bufferline

--- private_dot_config/nvim/lua/user/bufferline.lua
+++ private_dot_config/nvim/lua/user/bufferline.lua
@@ -5,9 +5,24 @@ end
 
 bufferline.setup {
   options = {
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    close_command = "Bdelete! %d",
+    right_mouse_command = "Bdelete! %d",
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+    separator_style = { " ", " " },
+    buffer_close_icon = "x",
+    close_icon = "x",
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    indicator = {
+      style = "none",
+    },
+  },
+  highlights = {
+    fill = { bg = "#1e1e2e" },
+    background = { bg = "#11111b" },
+    buffer_selected = { bg = "#11111b", bold = true, italic = true },
+    separator = { fg = "#1e1e2e", bg = "#1e1e2e" },
+    separator_selected = { fg = "#1e1e2e", bg = "#1e1e2e" },
+    separator_visible = { fg = "#1e1e2e", bg = "#1e1e2e" },
   },
 }
