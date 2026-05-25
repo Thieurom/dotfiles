72b74bfb91ea2c77783d6e224387df1b201733bc
Thieurom <doanthieu@MacBook-Pro.local>
2026-05-25 20:35:29 +0700

    Replace nvim-tree with neo-tree

---
+++ private_dot_config/nvim/lua/user/neo-tree.lua
@@ -0,0 +1,36 @@
+local status_ok, neo_tree = pcall(require, "neo-tree")
+if not status_ok then
+  return
+end
+
+neo_tree.setup {
+  close_if_last_window = true,
+  popup_border_style = "rounded",
+  enable_git_status = true,
+  enable_diagnostics = true,
+  filesystem = {
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = true,
+    },
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["h"] = "close_node",
+        ["l"] = "open",
+      },
+    },
+  },
+  window = {
+    position = "float",
+    width = 50,
+    mappings = {
+      ["q"] = "close_window",
+    },
+  },
+}
