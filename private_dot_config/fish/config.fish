9545baebf9d1d0db1b1dd39e71ee3a3e9162abbb
Thieurom <doanthieu@MacBook-Pro.local>
2026-05-26 20:59:08 +0700

    Fix fish CLI tool paths

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,8 +1,31 @@
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+if test -x /opt/homebrew/bin/mise
+    /opt/homebrew/bin/mise activate fish | source
+end
+
+for dir in \
+    $HOME/.asdf/shims \
+    $HOME/go/bin \
+    $HOME/.bin \
+    $HOME/.local/share/mise/shims \
+    $HOME/.local/bin
+    if test -d $dir
+        if set -l index (contains -i -- $dir $PATH)
+            set -e PATH[$index]
+        end
+        set -gx PATH $dir $PATH
+    end
+end
+
+set -gx EDITOR nvim
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux EDITOR nvim
 alias vi="nvim"
 alias vim="nvim"
 alias view="nvim -R"
